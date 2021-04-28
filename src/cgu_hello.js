1df7fb4df677a0036c509cb7fa04d5aa72130230
Tommmy <user@gmail.com>
2021-04-28 15:38:55 +0800

    first commit

---
+++ src/cgu_hello.js
@@ -0,0 +1,6 @@
+const styleArgument = { fontSize: '100px', color: 'blue' };
+
+const cgu_hello=()=>{
+ return <h1 style = { styleArgument } > hello CGU!! </h1>;
+}
+export default cgu_hello;
---
+++ src/cgu_multiButton.js
@@ -0,0 +1,20 @@
+const changeText=(event)=>{
+    console.log(event.target)
+    event.target.innerText = event.target.innerText + "白癡喔別點啦"
+   }
+   const MultiButton=(num)=>{
+    var output=[];
+    for(let i=1;i<num+1;++i)
+    output.push(<button onClick={changeText}>第{i}個按鍵</button>)
+    return output;
+   }
+   function App() {
+    return (
+    <div className="App">
+    { MultiButton(10) }
+    </div>
+    );
+   }
+
+   
+export default MultiButton;
